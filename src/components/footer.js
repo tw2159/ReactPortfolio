948d874f3656d18d5fad1c79834f6276131de56d
Te Wei <tw2159@nyu.edu>
2021-12-27 17:36:07 -0800

    update navigation

---
+++ src/components/footer.js
@@ -0,0 +1,32 @@
+function Footer() {
+  const icons = [
+    {
+      link: "mailto:tw2159@nyu.edu",
+      class: "fas fa-envelope"
+    },
+    {
+      link: "https://github.com/tw2159",
+      class: "fab fa-github"
+    },
+    {
+      link: "https://www.linkedin.com/in/te-w-9b18b157/",
+      class: "fab fa-linkedin"
+    }
+  ];
+
+  return(
+    <footer>
+      <ul>
+        {icons.map((icon, index) => 
+          <li>
+            <a href={icon.link} key={index}>
+              <i className={icon.class}></i>
+            </a>
+          </li>
+        )}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return(
+    <section className="home hero">
+      <h3>Fantastic Coding Journey In Bootcamp!</h3>
+    </section>
+  );
+}
+
+export default Home;
