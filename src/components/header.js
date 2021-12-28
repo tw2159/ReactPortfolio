b35deb83dfcde14fe6b6bed89cc3fb8b0a1d5daa
Te Wei <tw2159@nyu.edu>
2021-12-27 17:32:38 -0800

    update home

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import Navigation from "./navigation";
+
+function Header() {
+  return(
+    <header>
+      <div className="title"><a href="/home">Te Wei</a></div>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
